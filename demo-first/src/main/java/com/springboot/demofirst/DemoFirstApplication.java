10ef886e35fac84c1895884b54655a59ab4ee2c9
niuqingsong <niuqingsng@jd.com>
2021-03-31 18:11:57 +0800

    init

---
+++ demo-first/src/main/java/com/springboot/demofirst/DemoFirstApplication.java
@@ -0,0 +1,19 @@
+package com.springboot.demofirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableEurekaClient
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class DemoFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFirstApplication.class, args);
+	}
+
+}
